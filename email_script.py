5e6a65afabbdd5faffcbd3c2ab7e0dc9440d4265
Eitan Rothman <53872013+eitan-rothman@users.noreply.github.com>
2020-04-20 12:39:08 -0400

    Add files via upload

---
+++ email_script.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 23 12:17:00 2020
+
+pip install --upgrade google-api-python-client google-auth-httplib2 google-auth-oauthlib
+
+@author: Eitan
+"""
+
+def login():
+    
+    return 0
+
+def compose():
+    return 0
+
+def send():
+    return 0
+
+
+
+def main():
+    
+
+main()
